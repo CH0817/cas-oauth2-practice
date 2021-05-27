0023006793389c415d9b290f70e03b961e515dfa
RexYu <yu.chenhang@gmail.com>
2021-05-27 17:42:25 +0800

    init

---
+++ resource-server/src/main/java/tw/com/rex/resourceserver/ResourceServerApplication.java
@@ -0,0 +1,13 @@
+package tw.com.rex.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceServerApplication.class, args);
+    }
+
+}
